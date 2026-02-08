d799f7753f9a45dda0d6c4b852b20486d0724fa0
Dasango <dasango@uce.edu.ec>
2026-02-07 21:01:41 -0500

    Gradle in 1 min

---
+++ Libray/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("io.freefair.lombok") version "9.1.0"
+}
+
+group = "com.uce"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("org.jboss.weld.se:weld-se-core:6.0.3.Final")
+
+    implementation("org.postgresql:postgresql:42.7.8")
+
+    implementation("org.hibernate:hibernate-core:7.1.10.Final")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+sourceSets{
+    main{
+        output.setResourcesDir(output.classesDirs.files.first())
+    }
+}
